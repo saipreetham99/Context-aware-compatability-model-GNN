d8704c20cb9b1f402d202391d624a077bb419ca5
saipreetham99 <sai.preetham99@gmail.com>
2025-02-08 04:25:45 -0400

    Added utility files and dataloaders

---
+++ data/amazon/utils/prepare_dataset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+dir="dataset"
+
+metadata="metadata.pkl"
+if [ ! -f "$metadata" ]; then
+  cd utils
+  python parse_metadata.py
+  cd ..
+fi
+
+if [[ ! -e "$dir" ]]; then
+  cd utils
+  python create_dataset.py
+  cd ..
+fi
